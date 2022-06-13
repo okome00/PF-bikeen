72db5e88c81c7a3a9c52fbc675a23531a6141be6
EC2 Default User <ec2-user@ip-172-31-93-140.ec2.internal>
2022-06-13 13:59:18 +0000

    [Update]データ移行用

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -2,15 +2,15 @@ class User::FavoritesController < ApplicationController
   before_action :authenticate_user! ## ログイン権限
 
   def create ## いいね！投稿アクション
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    post.create_notification_favorite!(current_user)
+    @post.create_notification_favorite!(current_user)
   end
 
   def destroy ## いいね！削除アクション
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
   end
 
